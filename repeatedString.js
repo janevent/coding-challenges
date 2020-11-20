91795206ba3470b5fb03354de7c2f716c5e1cdf8
janeventer <janeanneventer@gmail.com>
2020-11-20 15:06:15 -0800

    add repeatedString challenge

---
+++ repeatedString.js
@@ -0,0 +1,25 @@
+function repeatedString(s, n) {
+    let ar = s.split("");
+    let numChar = ar.length;
+    let times = Math.floor(n/numChar);
+    let remainder = n % numChar;
+    let f = ar.filter( e => e === "a");
+    let numA = f.length;
+    
+    let countA = numA * times;
+    
+    let remainingA = 0;
+    let i = 0;
+    
+    while( i < remainder){
+        if(ar[i] === "a"){
+            remainingA++;
+        }
+        i++;
+    }
+
+    countA+=remainingA;
+    return countA;
+}
+
+console.log(repeatedString("alfalfa", 300))
