986f4d0ce52e72bc109ad7957c8b1bcbebc60ee6
janeventer <janeanneventer@gmail.com>
2020-12-27 23:22:58 -0800

    find maximum depth of a binary tree

---
+++ maximumDepth.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    if(root === null){
+        return 0;
+    }else if(root.left === null && root.right === null){
+        return 1;
+    }else{
+        let l = maxDepth(root.left);
+        let r = maxDepth(root.right);
+        return (1 + ((l > r) ? l : r) );
+    }
+
+};
